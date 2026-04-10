bdf33233f084563e7981bc7f2ee86b89364c955f
Rati Krishna Moharana <mratikrishna@gmail.com>
2026-04-10 21:50:23 +0530

    backend added

--- 02_mini_project/backend.tf
+++ 02_mini_project/backend.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "my-terraform-state-bucket-krishna"
-#     key    = "02_mini_project/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket-krishna"
+    key    = "02_mini_project/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
