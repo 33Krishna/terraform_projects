18a15dce97b8d8ed6bfaa371cd3d1442084bc2c9
Rati Krishna Moharana <mratikrishna@gmail.com>
2026-04-15 13:22:42 +0530

    More Attributes added

--- 03_mini_project/main.tf
+++ 03_mini_project/main.tf
@@ -5,19 +5,18 @@ data "aws_caller_identity" "current" {}
 resource "aws_iam_user" "users" {
 for_each = {
   for user in local.users : "${user.first_name}-${user.last_name}" => user
-}
+  }
 
   name = lower("${substr(each.value.first_name, 0, 1)}${each.value.last_name}")
   path = "/users/"
-
-tags = {
-  DisplayName = "${each.value.first_name} ${each.value.last_name}"
-  Department  = each.value.department
-  JobTitle    = each.value.job_title
-  Email       = each.value.email
-  Location    = each.value.location
-  AccessLevel = each.value.access_level
-}
+  tags = {
+    DisplayName = "${each.value.first_name} ${each.value.last_name}"
+    Department  = each.value.department
+    JobTitle    = each.value.job_title
+    Email       = each.value.email
+    Location    = each.value.location
+    AccessLevel = each.value.access_level
+  }
 }
 
 # Create IAM login profiles ( Password )
